7b0acdc0c04b7dd56624e1aca232fb217ef1250f
Leonardo Fadul <lsffadul@gmail.com>
2021-09-20 11:53:35 -0300

    Showing categories

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { CategorieDTO } from '../../models/categories.dto';
 import { CategorieService } from '../../services/domain/categorie.service';
 
 /**
@@ -16,6 +18,10 @@ import { CategorieService } from '../../services/domain/categorie.service';
 })
 export class CategoriesPage {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategorieDTO[];
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -25,7 +31,7 @@ export class CategoriesPage {
   ionViewDidLoad() {
     this.categorieService.findAll()
       .subscribe(response => {
-        console.log(response)
+        this.items = response;
       },
       error => {
         console.log(error);
